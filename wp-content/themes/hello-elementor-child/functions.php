80a5fb7223934baf9ebaa4b229530a5c40c59839
Pawel Struminski <pstruminski@gmail.com>
2026-03-16 14:37:10 +0100

    wp admin bar disable

--- wp-content/themes/hello-elementor-child/functions.php
+++ wp-content/themes/hello-elementor-child/functions.php
@@ -5,6 +5,16 @@
 
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_styles' );
 
+add_action( 'after_setup_theme', 'hello_elementor_child_disable_admin_bar' );
+
+function hello_elementor_child_disable_admin_bar() {
+	$user = wp_get_current_user();
+
+	if ( in_array( 'editor', (array) $user->roles, true ) ) {
+		show_admin_bar( false );
+	}
+}
+
 function hello_elementor_child_enqueue_styles() {
 	wp_enqueue_style(
 		'hello-elementor-child-style',
